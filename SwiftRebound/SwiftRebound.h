ccb54ae638c330091a85255d91fbf08f8d6f3519
Andrew Hunter <andrew@logicalshift.co.uk>
2016-07-10 09:28:46 +0100

    Add framework and unit tests

---
+++ SwiftRebound/SwiftRebound.h
@@ -0,0 +1,19 @@
+//
+//  SwiftRebound.h
+//  SwiftRebound
+//
+//  Created by Andrew Hunter on 10/07/2016.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftRebound.
+FOUNDATION_EXPORT double SwiftReboundVersionNumber;
+
+//! Project version string for SwiftRebound.
+FOUNDATION_EXPORT const unsigned char SwiftReboundVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftRebound/PublicHeader.h>
+
+
