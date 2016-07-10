fab895a990780fb3f78d2fed8f3461527bdfbe3c
Andrew Hunter <andrew@logicalshift.co.uk>
2016-07-10 13:33:14 +0100

    Implement the basic 'compute' operation on BoundComputable

--- SwiftRebound/Binding/BoundComputable.swift
+++ SwiftRebound/Binding/BoundComputable.swift
@@ -12,7 +12,20 @@ import Foundation
 /// Represents a bound item whose value is 
 ///
 internal class BoundComputable<TBoundType> : Bound<TBoundType> {
+    private let _compute: () -> TBoundType;
+    
     init(compute: () -> TBoundType) {
-        
+        _compute = compute;
+
+        super.init();
+    }
+    
+    ///
+    /// Recomputes the value of this bound object and returns the result
+    ///
+    /// Subclasses must override this to describe how a bound value is updated
+    ///
+    override func computeValue() -> TBoundType {
+        return _compute();
     }
 }
