02f8e67a6edd0d678dfbc6d76decc6372041d9f7
Andrew Hunter <andrew@logicalshift.co.uk>
2016-07-12 21:54:28 +0100

    Ensure that the trigger update function is only called the first time the trigger is invalidated.

--- SwiftRebound/Binding/Trigger.swift
+++ SwiftRebound/Binding/Trigger.swift
@@ -23,6 +23,9 @@ internal class Trigger : Changeable, Notifiable, Lifetime {
     
     /// Lifetime of the dependency change monitor
     private var _dependencyLifetime: Lifetime?;
+    
+    /// True if an update is pending (we only call the update function once when invalidated, and only call it again once the action runs)
+    private var _updatePending = false;
 
     init(action: () -> ()) {
         self.action = action;
@@ -33,12 +36,16 @@ internal class Trigger : Changeable, Notifiable, Lifetime {
     ///
     func performAction() {
         BindingContext.withNewContext {
+            // Expect the earlier set of dependencies
             let currentContext  = BindingContext.current!;
             let oldDependencies = self._dependencies;
             
             if let oldDependencies = oldDependencies {
                 currentContext.setExpectedDependencies(oldDependencies);
             }
+            
+            // Update is no longer pending
+            self._updatePending = false;
             
             // Run the action in the context
             self.action();
@@ -74,21 +81,26 @@ internal class Trigger : Changeable, Notifiable, Lifetime {
     /// The next time the value is resolved, it will register as a change and the observers will be called.
     ///
     func markAsChanged() {
-        var needToTidy = false;
-        
-        // Run any actions that result from this value being updated
-        for notificationWrapper in _actionsOnChanged {
-            if let action = notificationWrapper.target {
-                action.markAsChanged();
-            } else {
-                needToTidy = true;
+        if !_updatePending {
+            // If the update function hasn't caused the trigger to run again, don't call it again
+            _updatePending = true;
+
+            var needToTidy = false;
+            
+            // Run any actions that result from this value being updated
+            for notificationWrapper in _actionsOnChanged {
+                if let action = notificationWrapper.target {
+                    action.markAsChanged();
+                } else {
+                    needToTidy = true;
+                }
             }
-        }
-        
-        if needToTidy {
-            _actionsOnChanged = _actionsOnChanged.filter { notificationWrapper in
-                return notificationWrapper.target != nil
-            };
+            
+            if needToTidy {
+                _actionsOnChanged = _actionsOnChanged.filter { notificationWrapper in
+                    return notificationWrapper.target != nil
+                };
+            }
         }
     }
 
