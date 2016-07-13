4ec01836c048af196e49d6c51d622b2d26d7262b
Andrew Hunter <andrew@logicalshift.co.uk>
2016-07-13 21:17:37 +0100

    Don't need bindAction() - can achieve the equivalent thing using trigger()

--- SwiftRebound/Binding/Binding.swift
+++ SwiftRebound/Binding/Binding.swift
@@ -34,19 +34,6 @@ public class Binding {
     }
     
     ///
-    /// Binds an action function
-    ///
-    /// This returns a bound function and a Changeable that can be used to monitor it. The changeable will
-    /// notify `whenChanged` whenever any of the dependencies used by the last time through the bound function
-    /// are changed. (Note that it won't fire until the action function is run at least once)
-    ///
-    static func bindAction(action: () -> ()) -> (() -> (), Changeable) {
-        let trigger = Trigger(action: action);
-        
-        return (trigger.performAction, trigger);
-    }
-    
-    ///
     /// Creates a triggered action
     ///
     /// This is something like a drawing function where it can be triggered to update by calling `setNeedsDisplay()`.
