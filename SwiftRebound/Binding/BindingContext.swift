4ac2da14aeebd4fc0c093c556576ababfaec34c3
Andrew Hunter <andrew@logicalshift.co.uk>
2016-11-20 15:10:21 +0000

    Use a pool of binding context queues instead of a single queue

--- SwiftRebound/Binding/BindingContext.swift
+++ SwiftRebound/Binding/BindingContext.swift
@@ -8,15 +8,25 @@
 
 import Foundation
 
+///
+/// Key used to retrieve the binding context on the current queue
+///
 private let _contextSpecificName = DispatchSpecificKey<QueueBindingContext>();
 
 ///
-/// Binding context queue to use if the current queue is not already a binding queue
+/// Semaphore used to decide which context queue to use
 ///
-/// This is not used for the purposes of serialization but rather because creating new queues is bad for performance.
-/// This can have the unexpected consequence that all computed values are computed on the same queue.
+private let _queueSemaphore = DispatchSemaphore(value: 1);
+
 ///
-private let _defaultContextQueue = BindingContext.createQueueWithNewContext();
+/// Semaphore that is held as long as there are 0 queues available
+///
+private let _queueWaitSemaphore = DispatchSemaphore(value: 1);
+
+///
+/// Queues used to
+///
+private var _contextQueues = BindingQueuePool.createContextQueues(8);
 
 ///
 /// Stores the binding context for the current queue
@@ -26,6 +36,91 @@ private class QueueBindingContext {
     
     init(context: BindingContext) {
         self.context = context;
+    }
+}
+
+///
+/// Retrieves/returns binding context queues
+///
+private class BindingQueuePool {
+    ///
+    /// Creates a set of context queues
+    ///
+    static func createContextQueues(_ count: Int) -> [DispatchQueue] {
+        var result = [DispatchQueue]();
+        
+        for _ in 0..<count {
+            let queue = BindingQueuePool.createQueueWithNewContext();
+            result.append(queue);
+        }
+        
+        return result;
+    }
+    
+    ///
+    /// Creates a new dispatch queue with a new binding context
+    ///
+    static func createQueueWithNewContext() -> DispatchQueue {
+        // Generate a new context
+        let newContext  = BindingContext();
+        
+        // Create a queue to use the context in
+        let queue       = DispatchQueue(label: "io.logicalshift.binding", attributes: []);
+        let storage     = QueueBindingContext(context: newContext);
+        
+        queue.setSpecific(key: _contextSpecificName, value: storage);
+        
+        return queue;
+    }
+    
+    ///
+    /// Retrieves a context queue, or waits for one to become available
+    ///
+    static func retrieveContextQueue() -> DispatchQueue {
+        // Acquire the semaphore
+        _queueSemaphore.wait();
+        defer { _queueSemaphore.signal(); }
+        
+        // If there are no queues available, then wait for someone to signal the wait semaphore
+        while _contextQueues.count <= 0 {
+            // Release the queue semaphore so that queues can be returned
+            _queueSemaphore.signal();
+            
+            // Wait for the wait semaphore to be signaled (indicates a queue was returned to the pool)
+            _queueWaitSemaphore.wait();
+            _queueWaitSemaphore.signal();
+            
+            // Re-acquire the queue semaphore so we can check for queues
+            _queueSemaphore.wait();
+        }
+        
+        // If we got the semaphore, there is always at least one queue available in the list
+        let queue = _contextQueues.popLast()!;
+        
+        // If we just got the last queue, then acquire the wait semaphore so we can block anything waiting for a queue
+        if _contextQueues.count == 0 {
+            _queueWaitSemaphore.wait();
+        }
+        
+        // Release the semaphore and return the result
+        return queue;
+    }
+    
+    ///
+    /// Returns a context queue to the pool
+    ///
+    static func returnContextQueue(_ queue: DispatchQueue) {
+        // Acquire the semaphore
+        _queueSemaphore.wait();
+        defer { _queueSemaphore.signal(); }
+        
+        // If this is the first queue back in the pool, then wake any threads waiting for more queues
+        if _contextQueues.count == 0 {
+            _queueWaitSemaphore.signal();
+        }
+        
+        // Return queue to the pool
+        _contextQueues.append(queue);
     }
 }
 
@@ -67,22 +162,6 @@ public class BindingContext {
     }
     
     ///
-    /// Creates a new dispatch queue with a new binding context
-    ///
-    public static func createQueueWithNewContext() -> DispatchQueue {
-        // Generate a new context
-        let newContext  = BindingContext();
-        
-        // Create a queue to use the context in
-        let queue       = DispatchQueue(label: "io.logicalshift.binding", attributes: []);
-        let storage     = QueueBindingContext(context: newContext);
-        
-        queue.setSpecific(key: _contextSpecificName, value: storage);
-        
-        return queue;
-    }
-    
-    ///
     /// Creates a new binding context (which can be retrieved with current) and performs the specified action with
     /// it in effect
     ///
@@ -98,9 +177,12 @@ public class BindingContext {
             action();
             existingStorage.context = oldContext;
         } else {
-            // Current queue doesn't have any context stored, move on to the default queue
+            // Current queue doesn't have any context stored, move on to a queue
             // Could also call createQueueWithNewContext() here, but that is slow
-            _defaultContextQueue.sync(execute: {
+            let queue = BindingQueuePool.retrieveContextQueue();
+            defer { BindingQueuePool.returnContextQueue(queue); }
+            
+            queue.sync(execute: {
                 let existingStorage = BindingContext.currentStorage!;
                 
                 // Generate a new context
